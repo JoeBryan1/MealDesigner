d2acbf81f11ded53133d6bb8ec386f4134086156
Joseph <45214455+JoeBryan1@users.noreply.github.com>
2024-12-08 17:34:11 +0000

    Pointed front-end back to azure web api

--- MealDesigner.Client/src/services/PromptService.ts
+++ MealDesigner.Client/src/services/PromptService.ts
@@ -12,7 +12,7 @@ export type PromptResponse = {
 
 export default class PromptService {
     async TriggerFoodImageGen(foodItemArray: FoodItem[]) : Promise<PromptResponse> {
-        return await fetch('http://localhost:5244/api/prompt', {
+        return await fetch('https://meal-designer-api-fshcgpckhyfpf9bv.uksouth-01.azurewebsites.net/api/prompt', {
             method: 'POST',
             headers: {
                 'Accept': '*/*',

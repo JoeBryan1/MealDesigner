2c0aef45240a8213e94fd31de59225540285d590
Joseph <45214455+JoeBryan1@users.noreply.github.com>
2024-12-14 21:58:39 +0000

    Added some more information text to home

--- MealDesigner.Client/src/pages/Home.tsx
+++ MealDesigner.Client/src/pages/Home.tsx
@@ -10,31 +10,46 @@ export class Home extends Component {
     render() {
         return (
             <div>
-                <h1 className={"text-3xl font-bold pb-3"}>Welcome to Meal Designer AI – Your Personal Culinary
-                    Innovator!</h1>
-                <ul className="list-disc pl-10 pb-3">
-                    <li className={"py-1"}>
-                        Discover the joy of creating unique recipes with ease.
-                    </li>
-                    <li className={"py-1"}>
-                        Simply choose your favorite ingredients, and our intelligent AI will whip up a delicious, custom
-                        recipe just for you.
-                    </li>
-                    <li className={"py-1"}>
-
-                        Need a visual taste? We’ll generate a stunning image of your dish to inspire your inner chef.
-                    </li>
-                    <li className={"py-1"}>
-                        Whether you're working with a few pantry staples or experimenting with exotic flavors, AI Recipe
-                        Creator makes cooking fun, creative, and hassle-free.
-                        Let’s turn your ingredients into magic today!
-                    </li>
-                </ul>
+                <div>
+                    <h1 className={"text-3xl font-bold pb-3"}>Welcome to Meal Designer AI – Your Personal Culinary
+                        Innovator!</h1>
+                        
+                    <ul className="list-disc pl-10 pb-3">
+                        <li className={"py-1"}>
+                            Discover the joy of creating unique recipes with ease.
+                        </li>
+                        <li className={"py-1"}>
+                            Simply choose your favorite ingredients, and our intelligent AI will whip up a delicious, custom
+                            recipe just for you.
+                        </li>
+                        <li className={"py-1"}>
+    
+                            Need a visual taste? We’ll generate a stunning image of your dish to inspire your inner chef.
+                        </li>
+                        <li className={"py-1"}>
+                            Whether you're working with a few pantry staples or experimenting with exotic flavors, AI Recipe
+                            Creator makes cooking fun, creative, and hassle-free.
+                            Let’s turn your ingredients into magic today!
+                        </li>
+                    </ul>
+                </div>
                 
+                <div>
+                    <h2 className={"text-xl font-bold pb-3"}>Caution</h2>
+                    <p>
+                        This is not a final product, this is a piece of portfolio work. 
+                        Also, be aware that using this costs me money.
+                        Each generation costs me about 10 US cents.
+                        I have not yet implemented a cap on the amounts of generation you can cause.
+                        Please be mindful, thanks.
+                    </p>
+                </div>
                 
-                <Link to="/create">
-                    <Button>Get Started</Button>
-                </Link>
+                <div className="pt-5">
+                    <Link to="/create">
+                        <Button>Get Started</Button>
+                    </Link>
+                </div>
                 
                 
             </div>
